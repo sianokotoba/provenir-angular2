07b57ff4bb9fe0dce3180011a48614ee465863d6
Sia <schen22@gmail.com>
2017-07-15 22:12:33 -0400

    form corrected, formControlName iterating for length of this.coffees

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,43 +19,43 @@ export class AppComponent implements OnInit, OnChanges {
   }
 
   coffees = [
-  { id: 1,
-    name: 'Cappuccino',
-    type: 'Hot',
-    displayText: 'Espresso and milk, topped with milk foam',
-    imgURL: '../assets/coffee-cappuccino.png'
-  },
-  { id: 2,
-    name: 'Americano',
-    type: 'Hot',
-    displayText: 'A mix of Espresso and agua (water!)',
-    imgURL: '../assets/coffee-americano.png'
-  },
-  { id: 3,
-    name: 'Espresso',
-    type: 'Hot',
-    displayText: 'The origin of many a coffee drink',
-    imgURL: '../assets/coffee-espresso.png'
-  },
-  { id: 4,
-    name: 'Cafe Macchiato',
-    type: 'Hot',
-    displayText: 'Espresso topped with lots of milk foam',
-    imgURL: '../assets/coffee-macchiato.png'
-  },
-  { id: 5,
-    name: 'Mocha',
-    type: 'Hot',
-    displayText: 'Espresso + chocolate syrup and lots of milk',
-    imgURL: '../assets/coffee-mocha.png'
-  },
-  { id: 6,
-    name: 'Latte',
-    type: 'Hot',
-    displayText: 'Espresso and lots of milk',
-    imgURL: '../assets/coffee-latte.png'
-  },
-];
+    { id: 1,
+      name: 'Cappuccino',
+      type: 'Hot',
+      displayText: 'Espresso and milk, topped with milk foam',
+      imgURL: '../assets/coffee-cappuccino.png'
+    },
+    { id: 2,
+      name: 'Americano',
+      type: 'Hot',
+      displayText: 'A mix of Espresso and agua (water!)',
+      imgURL: '../assets/coffee-americano.png'
+    },
+    { id: 3,
+      name: 'Espresso',
+      type: 'Hot',
+      displayText: 'The origin of many a coffee drink',
+      imgURL: '../assets/coffee-espresso.png'
+    },
+    { id: 4,
+      name: 'Cafe Macchiato',
+      type: 'Hot',
+      displayText: 'Espresso topped with lots of milk foam',
+      imgURL: '../assets/coffee-macchiato.png'
+    },
+    { id: 5,
+      name: 'Mocha',
+      type: 'Hot',
+      displayText: 'Espresso + chocolate syrup and lots of milk',
+      imgURL: '../assets/coffee-mocha.png'
+    },
+    { id: 6,
+      name: 'Latte',
+      type: 'Hot',
+      displayText: 'Espresso and lots of milk',
+      imgURL: '../assets/coffee-latte.png'
+    },
+  ];
 
   ngOnInit() {
     // Check local storage on init for correct cofee list
